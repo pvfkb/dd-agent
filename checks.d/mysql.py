5b8a1a38f9f18a73b95f421eda03b2c45289f024
pvfkb <patrickbarker@gmail.com>
2015-12-30 18:33:41 -0600

    Update mysql.py

--- checks.d/mysql.py
+++ checks.d/mysql.py
@@ -95,7 +95,7 @@ class MySql(AgentCheck):
         tags = instance.get('tags', None)
         options = instance.get('options', {})
         queries = instance.get('queries', [])
-        ssl = instance.get('ssl', {}) 
+        ssl = instance.get('ssl', {})
         return host, port, user, password, mysql_sock, defaults_file, tags, options, queries, ssl
 
     def _connect(self, host, port, mysql_sock, user, password, defaults_file, ssl):
@@ -129,7 +129,8 @@ class MySql(AgentCheck):
                 host=host, 
                 user=user, 
                 passwd=password, 
-                ssl=dict(ssl))
+                ssl=dict(ssl)
+                )
             else:
                 db = pymysql.connect(
                     host=host,
